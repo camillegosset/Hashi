6f93661a628d3dfe1a229480875874ddb4619cb0
TDiGiovanni <digio.thomas@hotmail.fr>
2017-01-27 15:59:24 +0100

    Ajout de Pont.cpp

---
+++ src/Pont.cpp
@@ -0,0 +1,43 @@
+#include "../inc/Pont.hpp"
+
+// Constructeur
+Pont::Pont(Ile i1, Ile i2) {
+    *_ile1= i1;
+    *_ile2= i2;
+    _nombre= 0;
+    _est_vertical= false;
+}
+
+// Accesseurs en lecture
+Ile Pont::getIle1() {
+    return _ile1;
+}
+
+Ile Pont::getIle2() {
+    return _ile2;
+}
+
+int Pont::getNombre() {
+    return _nombre;
+}
+
+bool Pont::getEstVertical() {
+    return _est_vertical;
+}
+
+// Accesseurs en écriture
+void Pont::setIle1(ile i) {
+    _ile1= i;
+}
+
+void Pont::setIle2(ile i) {
+    _ile2= i;
+}
+
+void Pont::setIle1(int n) {
+    _nombre= n;
+}
+
+void Pont::setEstVertical(bool v) {
+    _est_vertical= v;
+}
