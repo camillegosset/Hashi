db6d417ffed1f0a03f5b2a4d1190b44e8e51da0f
jeremie-dautheribes <je.je.laeti@gmail.com>
2017-02-01 15:23:55 +0100

    Suppression de lignes inutiles

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,6 @@ int main(int argc, char const *argv[]) {
     std::ifstream fichier(argv[1], std::ios::in);
 
     grille.lecture(fichier);
-
-    //std::cout<<"Longueur: "<<grille.getN()<<", Hauteur: "<<grille.getM()<<", valeur des iles: "<<grille.getUneIleOuUnPont(1,1).getIle()->getVal()<<", "<<grille.getUneIleOuUnPont(10,9).getIle()->getVal()<<", "<<grille.getUneIleOuUnPont(4,7).getIle()->getVal()<<", "<<grille.getUneIleOuUnPont(2,8).getIle()->getVal()<<'\n';
-    //std::cout << grille.getUneIleOuUnPont().getIle()->getVal() << '\n';
     grille.affichage(std::cout);
     return 0;
 }
