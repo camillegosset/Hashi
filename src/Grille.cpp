acec22a5d73c0ebf5d00e4bc85c1657181bdc9fb
Gosset Camille <camille.gosset@etu.umontpellier.fr>
2017-02-27 19:57:50 +0100

    Développement du 1er cas pour placer 1 pont

--- src/Grille.cpp
+++ src/Grille.cpp
@@ -312,7 +312,7 @@ void Grille::placerPonts(Ile* ile){
 
 void Grille::creerPont(Ile* ile1, Ile ile2, int nbr_ponts){
     Pont* pont= new Pont(ile1, _objets_presents[ile2.getX()][ile2.getY()].getIle(), nbr_ponts);
-    if( ile1->getX() == ile2.getX()){
+    if( ile1->getX() == ile2.getX()){ //Possibilité de savoir dès maintenant si vertical ou non donc on peut faire le CONSTRUCTEUR adéquate
         for(int i = ile1->getY() ; i < ile2.getY(); i++){
             //On crée un pont à chaque case
             _objets_presents[ile1->getX()][i].setPont(pont);
